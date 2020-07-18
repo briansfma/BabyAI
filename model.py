dfa9c60f8e12050f1fa6b776122793325930f0d8
briansfma <briansfma@gmail.com>
2020-07-18 02:26:32 -0700

    feat: implemented simple NN, manual backprop in preparation for RL experiment

--- model.py
+++ model.py
@@ -1,17 +1,47 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from numba import jit
 
-# Activation Functions
-def relu(x):
-    return np.maximum(x, 0)
+
+class SimEnviron:
+  def __init__(self):
+    self.heading = 0.
+    self.s = 0.
+    self.old_s = 0.
+
+    # self.maxspeed = 0.
+    # self.speed = 0.
+    # self.heading = 0.
+    # self.dist = 0.
+    # self.s = 0.
+
+  def reward(self, steering):
+    # Easy base case test
+    reward = 0 - steering
+
+    '''
+    TODO: The more fun model will include steering as an effector of heading and thus side displacement s
+    '''
+    # # Handle throttle/brake effect on speed
+    # self.speed += throttle - 0.1 * (self.speed)**2  # Approximate drag/friction
+    # self.speed = np.maximum(self.speed, 0)              # Clamp min value to 0 (stopped)
+    # self.maxspeed += 1 - 0.1 * (self.speed)**2
+    #
+    # # Handle steering effect on heading
+    # self.heading += self.speed * steering   # Discrete approximation
+    #
+    # # Handle net effect on car's position
+    # interval = self.speed * np.cos(self.heading)
+    # self.dist += interval
+    # self.s += self.speed * np.sin(self.heading)
+
+    return reward
+
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+  return 1 / (1 + np.exp(-x))
 
-def tanh(x):
-  return np.tanh(x)
 
-# Neuron types (classified by activation for now)
 class SigmoidNeuron:
   def __init__(self, weights, bias):
     self.weights = weights
@@ -20,108 +50,90 @@ class SigmoidNeuron:
   def feedforward(self, inputs):
     # Weight inputs, add bias, then use the activation function
     total = np.dot(self.weights, inputs) + self.bias
-    return sigmoid(total)
+    return np.asscalar(sigmoid(total))
 
-class ReLuNeuron:
-  def __init__(self, weights, bias):
-    self.weights = weights
-    self.bias = bias
-
-  def feedforward(self, inputs):
-    # Weight inputs, add bias, then use the activation function
-    total = np.dot(self.weights, inputs) + self.bias
-    return relu(total)
-
-class TanhNeuron:
-  def __init__(self, weights, bias):
-    self.weights = weights
-    self.bias = bias
-
-  def feedforward(self, inputs):
-    # Weight inputs, add bias, then use the activation function
-    total = np.dot(self.weights, inputs) + self.bias
-    return tanh(total)
 
 class SimpleNN:
   def __init__(self):
     # He initialization for hidden ReLus, Xavier init for output Tanh
-    weights_h1 = np.random.randn(1, 3) * np.sqrt(2/3)
-    weights_h2 = np.random.randn(1, 3) * np.sqrt(2/3)
-    weights_o = np.random.randn(1, 2) * np.sqrt(1/2)
-    bias_h1 = np.random.randn() * 0.1
-    bias_h2 = np.random.randn() * 0.1
-    bias_o = np.random.randn() * 0.1
+    # Case X = [X1 X2 X3]: weights_h1 = np.random.randn(1, 3) * np.sqrt(2/3)
+    w_h1 = np.random.randn(1, 2)[0] * np.sqrt(1 / 2)
+    w_h2 = np.random.randn(1, 2)[0] * np.sqrt(1 / 2)
+    w_out = np.random.randn(1, 2)[0] * np.sqrt(1 / 2)
+    b_h1 = np.random.randn() * 0.1
+    b_h2 = np.random.randn() * 0.1
+    b_out = np.random.randn() * 0.1
 
     # The Neuron class here is from the previous section
-    self.h1 = ReLuNeuron(weights_h1, bias_h1)
-    self.h2 = ReLuNeuron(weights_h2, bias_h2)
-    self.o = TanhNeuron(weights_o, bias_o)
+    self.h1 = SigmoidNeuron(w_h1, b_h1)
+    self.h2 = SigmoidNeuron(w_h2, b_h2)
+    self.out = SigmoidNeuron(w_out, b_out)
+
+    self.result_h1 = 0
+    self.result_h2 = 0
+    self.result_out = 0
 
   def feedforward(self, x):
-    out_h1 = self.h1.feedforward(x)
-    out_h2 = self.h2.feedforward(x)
-    out_o = self.o.feedforward([out_h1, out_h2])
+    self.result_h1 = self.h1.feedforward(x)
+    self.result_h2 = self.h2.feedforward(x)
+    self.result_out = self.out.feedforward([self.result_h1, self.result_h2])
 
-    # print("Out_h1: {}, Out_h2: {}, Out_o: {}".format(out_h1, out_h2, out_o))
+    # print("H1 = {}, H2 = {}, Out = {}".format(self.result_h1, self.result_h2, self.result_out))
 
-    return out_o
+    return self.result_out
 
-  def train(self):
-    pass
+  def train(self, x, error, learnrate, learnrate_bias=0):
+    # Set bias learn rate == learn rate if not explicitly provided
+    if learnrate_bias == 0:
+      learnrate_bias = learnrate
 
-# Generate a reward value for given steering/throttle input.
-# Rough approximation now for early training
-class SimEnviron:
-  def __init__(self):
-    self.maxspeed = 0.
-    self.speed = 0.
-    self.heading = 0.
-    self.dist = 0.
-    self.s = 0.
+    del_wout = error * (self.result_out * (1 - self.result_out))
+    self.out.weights += learnrate * del_wout * np.array([self.result_h1, self.result_h2])
+    self.out.bias += learnrate_bias * del_wout
 
-  def reward(self, steering, throttle):
+    del_wh1 = del_wout * self.out.weights[0] * (self.result_h1 * (1 - self.result_h1))
+    self.h1.weights += learnrate * del_wh1 * x
+    self.h1.bias += learnrate_bias * del_wh1
 
-    # Handle throttle/brake effect on speed
-    self.speed += throttle - 0.1 * (self.speed)**2  # Approximate drag/friction
-    self.speed = np.maximum(self.speed, 0)              # Clamp min value to 0 (stopped)
-    self.maxspeed += 1 - 0.1 * (self.speed)**2
+    # print(self.out.weights)
 
-    # Handle steering effect on heading
-    self.heading += self.speed * steering   # Discrete approximation
-
-    # Handle net effect on car's position
-    interval = self.speed * np.cos(self.heading)
-    self.dist += interval
-    self.s += self.speed * np.sin(self.heading)
-
-    # Report Progress
-    print("  Speed: {:06.4f}, Heading: {:06.4f}, D: {:06.4f}, S: {:06.4f}"
-          .format(self.speed, self.heading, self.dist, self.s))
-
-    # Assign a reward value
-    # interval: the closer to theoretical max, the more forward progress we're making.
-    # self.s: the closer to 0, the closer we are sticking to the path.
-    d_loss = np.exp(-2 * (self.maxspeed - interval))
-    s_loss = np.exp(-2 * self.s)
-    reward = d_loss + s_loss
-
-    print(reward)
-    return reward
+    del_wh2 = del_wout * self.out.weights[1] * (self.result_h2 * (1 - self.result_h2))
+    self.h2.weights += learnrate * del_wh2 * x
+    self.h2.bias += learnrate_bias * del_wh2
 
 
-# Let's play!
-environment = SimEnviron()
-calcSteer = SimpleNN()
-calcThrottle = SimpleNN()
+'''
+Simple test script for Neural Network functionality
+'''
 
-# Initial conditions
-x = np.array([0., 0., 0.])   # x1 = Dist, x2 = last Steering, x3 = last Throttle
+# Target Value
+y = np.array(0.5)
 
-# Do the feedforward loops
-for i in range(3):
-  steer_out = np.asscalar(calcSteer.feedforward(x))
-  throt_out = np.asscalar(calcThrottle.feedforward(x))
+# Constant Inputs (Only for testing, for now)
+x = np.array([1, 2])
 
-  print("Steering = {:06.4f}, Throttle = {:06.4f}".format(steer_out, throt_out))
+# Initialize network
+network = SimpleNN()
 
-  environment.reward(steer_out, throt_out)
+# Initialize environment
+sim = SimEnviron()
+
+point_xs = []
+point_ys = []
+
+for i in range(100):
+  # Calculate one gradient descent step for each weight
+  steering = network.feedforward([sim.s, sim.heading]) - 0.5  # Sigmoid output centers at y == 0.5
+  reward = sim.reward(steering)
+  error = reward
+
+  print('N{}: Steering: {}, \tReward: {}, \tError: {}'.format(i + 1, steering, reward, error))
+  print('\tS: {},  \t\tHeading: {}'.format(sim.s, sim.heading))
+
+  point_xs.append(i + 1)
+  point_ys.append(steering)
+
+  network.train(x, error, learnrate=1)
+
+plt.plot(point_xs, point_ys)
+plt.show()
